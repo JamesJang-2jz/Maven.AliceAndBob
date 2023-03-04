4ac4a8abdc764cc83597a2a9a765dc12e4a48e78
James <james@Zipcoders-MacBook-Pro-101.local>
2023-03-04 17:24:38 -0500

    james made changos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+        String alice = "Alice";
+        String bob = "Bob";
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What do they call you?");
+        String input = scan.nextLine();
+
+        if (input.equalsIgnoreCase(alice)) {
+            System.out.println("Bongiorno " + input + "!, letsa go get some gelato!");
+        } else if (input.equalsIgnoreCase(bob)) {
+            System.out.println("Hello " + input + ", would you like corn on the cob?");
+        } else {
+            System.out.println("Imposter, goodbye");
+        }
 
     }
 }
